cc186682589a688ac4c917b86547341516c6f3cf
nikhilrajbhar <nikhilrajbhar013@gmail.com>
2024-07-04 19:34:25 +0530

    Revert "third commit"
    This reverts commit 29bc70f33f6f85337a03fd1362a3ea1174ab2be5.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class. tetsing 3 4
+    //Add 'implements OnInit' to the class. tetsing 4
     const observables = new Observable((obj) => obj.next(Math.random()));
 
     //subsrciber1
