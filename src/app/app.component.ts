9b67f138e838f1bfe64a47d8f35c721254c17b27
nikhilrajbhar <nikhilrajbhar013@gmail.com>
2024-07-04 19:35:18 +0530

    adding line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class. tetsing 4
+    // adding line
     const observables = new Observable((obj) => obj.next(Math.random()));
 
     //subsrciber1
